b7a65657c5e39ca37eb6496649b46b85e8eb74fc
SuperAier <lldpf12@163.com>
2024-09-26 16:05:08 +0800

    push origin

--- src-tauri/src/app/cmd/download.rs
+++ src-tauri/src/app/cmd/download.rs
@@ -17,7 +17,6 @@ pub async fn download_img(name: String, blob: Vec<u8>) -> Result<String, String>
 #[tauri::command]
 pub async fn download_report(src_path: String, dest_path: String) -> Result<(), String> {
   println!("download report from ======={} to ======={}", src_path, dest_path); 
-  // info!("download report from ======={} to ======={}", src_path, dest_path); 
   // 检查源文件是否存在
   if !Path::new(&src_path).exists() {
       return Err("源文件不存在".to_string());
